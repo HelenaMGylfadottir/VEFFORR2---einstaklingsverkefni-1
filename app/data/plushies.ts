daba1949b9f7dfe03fef15165226b4912fadbfcc
HelenaMGylfadottir <161894419+HelenaMGylfadottir@users.noreply.github.com>
2026-01-17 21:30:54 +0000

    Update plushies.ts
    EVERYTHING CHANGED
    - information has been updated

--- app/data/plushies.ts
+++ app/data/plushies.ts
@@ -2,77 +2,114 @@ import { Plushie } from "../types/plushie";
 
 const plushies: Plushie[] = [
   {
-    id: "plush-1",
-    name: "Plush 1",
-    image: "/placeholder.jpeg",
-    obtained: "Obtained as a gift.",
-    age: "Had for a few years.",
-    funFact: "Always sits on the bed.",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    id: "marsh",
+    name: "Marsh",
+    image: "/marsh.png",
+    obtained: "Jólagjöf frá bróður mínum, fyrir mörgum árum.",
+    age: "10 ára (2016)",
+    funFact: "Foreldrarnir mínir hjálpuðu bróður mínum að velja þessa gjöf með því að herma eftir MÉR (ég keypti bangsa frá sömu búð handa honum).",
+    about: "Bangsi sem ég fékk um jólin árið 2016 frá bróður mínum. Ég skírði hann Marsh því hann er í lagin eins og sykurpúði (rosa frumleg). Hann hefur ferast með mér mikið gegn um árin og er enn alveg jafn mjúkur :D"
   },
   {
-    id: "plush-2",
-    name: "Plush 2",
-    image: "/placeholder.jpeg",
-    obtained: "Bought during a trip.",
-    age: "About two years old.",
-    funFact: "Very soft and fluffy.",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    id: "toby",
+    name: "Toby",
+    image: "/toby.png",
+    obtained: "Keyptur á flugvelli á leiðinni heim til Íslands.",
+    age: "6 ára (2020)",
+    funFact: "Fulla nafnið hans er Tobias!",
+    about: "Bangsi sem ég átti upprunalega ekkert að kaupa. Ég hafði lofað mér sjálfri að ekki kaupa neina bangsa í þessari ferð sem ég fór með fjölskyldu minni í, og mér gekk bara svaka vel. En á flugvöllinum kom ég auga á þennan bangsa og það var ást að fyrstu sín. Ég þurfti að fá hann. Mamma var ekki mjög sátt um það fyrst en með tímanum hefur það skánað, og nú hefur Toby komið með mér í fleiri ferðir!"
   },
   {
-    id: "plush-3",
-    name: "Plush 3",
-    image: "/placeholder.jpeg",
-    obtained: "Found in a small shop.",
-    age: "Less than a year old.",
+    id: "albert",
+    name: "Albert",
+    image: "/albert.png",
+    obtained: "Keyptur í IKEU af foreldrum mínum.",
+    age: "11 ára (2015)",
     funFact: "Has a tiny stitched smile.",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    about: "Bangsi sem var hannaður af 8 ára krakka sem partur af árlegu teiknikeppni Ikeu. Albert var fyrst keyptur handa bróður mínum, en nokkrum mánuðum seinna var hann nú þegar búin að fá nóg af böngsum og þá fékk ég að hafa hann í staðinn. Albert er þægilegur að halda á. :|"
   },
   {
-    id: "plush-4",
-    name: "Plush 4",
-    image: "/placeholder.jpeg",
-    obtained: "A surprise present.",
-    age: "Owned for many years.",
-    funFact: "Has been on many trips.",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    id: "hubert",
+    name: "Hubert",
+    image: "/hubert.png",
+    obtained: "Pabbi minn keypti hann með mér í IKEU.",
+    age: "1.5 ára (2024)",
+    funFact: "Flestar býflugur sem þú sérð fljúga um eru í raun kvenkyns og hafa brodd! Karlkyns býflugur hafa ekki broddi og þar sem þessi bangsi hefur engan nefndi ég hann Hubert.",
+    about: "Ég hafði séð þennan bangsa áður í IKEU en mér tókst að standast freistinguna að kaupa einn nokkrum sinnum þegar ég var í IKEA með foreldrum mínum. En einn daginn með pabba mínum ákvað ég loks að kaupa einn (ég elska nefnilega pöddur). Ég ætlaði að borga fyrir hann sjálf en pabbi minn náði að ýta mér frá og borga fyrir mig á síðustu sekúndu."
   },
   {
-    id: "plush-5",
-    name: "Plush 5",
-    image: "/placeholder.jpeg",
-    obtained: "Bought online.",
-    age: "Around three years old.",
-    funFact: "Perfect size for hugging.",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    id: "bingle",
+    name: "Bingle",
+    image: "/bingle.png",
+    obtained: "Keypti hann í nytjamarkaði.",
+    age: "Minna en ár (2025)",
+    funFact: "Vinur minn skýrði bangsan! :D",
+    about: "Ég fór út í nytjamarkaðinn fyrir jólinn á seinasta ári, ætlaði bara að skoða mig um. Það síðasta sem ég vildi var að kaupa nýjan bangsa en ég sá hann þarna, og fékk svo mikið samviskubit í búðinni og þurfti að grípa hann, kostaði bara 300 kr og ég elska hann mjög mikið! Hann hefur bara 3 set af fótleggjum, en krabbar eru yfirleitt með 4! Svo ég fékk þá hugmynd að hann missti fætur í fyrri fellingu (molt) og að þeir hefðu ekki vaxið aftur ennþá, það er afsökunin mín á ónákvæmri hönnun bangsans haha."
   },
   {
-    id: "plush-6",
-    name: "Plush 6",
-    image: "/placeholder.jpeg",
-    obtained: "Won as a prize.",
-    age: "About a year old.",
-    funFact: "Has slightly mismatched eyes.",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    id: "appi",
+    name: "Appí",
+    image: "/appi.png",
+    obtained: "Ein af fyrstu gjöfum sem ég fékk þegar ég fæddist.",
+    age: "18 ára (2007)",
+    funFact: "Ef maður lyftir skottið hans og eyru frá líkamanum sér maður förinn eftir þau",
+    about: "Ég fékk þennan bangsa og einn annan (Kanínna) daginn sem ég fæddist frá ömmu og afa mínum. Um leið og ég var nógu gömul til að tala smá skýrði ég þá 'Appí og Kanínna' (borið svona fram. Svaka frumlegt hjá mér). Þau voru óaðskiljanleg frá mér, það virtist smá eins og ég gæti ekki starfað án þeirra. T.d þegar þeir voru settir í þvottavélina myndi ég sitja fyrir framan hana allan þennan tíma og svo gera það sama meðan þeir þornuðu þar til ég gat haldið á þeim aftur. Ég elska þá innilega og ég er svo glöð að ég á þá ennþá."
   },
   {
-    id: "plush-7",
-    name: "Plush 7",
-    image: "/placeholder.jpeg",
-    obtained: "Gift from a friend.",
-    age: "Several years old.",
-    funFact: "Always faces the window.",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    id: "kaninna",
+    name: "Kanínna",
+    image: "/kaninna.png",
+    obtained: "Ein af fyrstu gjöfum sem ég fékk þegar ég fæddist.",
+    age: "18 ára (2007)",
+    funFact: "Hann er mýkri en hann lítur út fyrir að vera",
+    about: "Ég fékk þennan bangsa og einn annan (Appí) daginn sem ég fæddist frá ömmu og afa mínum. Um leið og ég var nógu gömul til að tala smá skýrði ég þá 'Appí og Kanínna' (borið svona fram. Svaka frumlegt hjá mér). Þau voru óaðskiljanleg frá mér, það virtist smá eins og ég gæti ekki starfað án þeirra. T.d þegar þeir voru settir í þvottavélina myndi ég sitja fyrir framan hana allan þennan tíma og svo gera það sama meðan þeir þornuðu þar til ég gat haldið á þeim aftur. Ég elska þá innilega og ég er svo glöð að ég á þá ennþá."
   },
   {
-    id: "plush-8",
-    name: "Plush 8",
-    image: "/placeholder.jpeg",
-    obtained: "Bought on a whim.",
-    age: "Recently acquired.",
-    funFact: "Has the softest fabric.",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    id: "vuffi",
+    name: "Vuffi",
+    image: "/vuffi.png",
+    obtained: "Gjöf frá foreldrum mínum frá því að ég var mjög lítil.",
+    age: "15 ára (2011)",
+    funFact: "Þessi bangsi á eiginlega ekki nafn, sem er ólíkt mér. Ég fór og spurði mömmu mína um það og hún sagði að henni minnti að ég hafi kallað hann 'Vuffi', eða eitthvað þannig.",
+    about: "Ég man lítið um hvenær ég fékk hann en ég hef elskað þennan bangsa síðan að ég fékk hann."
+  },
+  {
+    id: "zote",
+    name: "Zote",
+    image: "/zote.png",
+    obtained: "Fékk sem afmælisgjöf frá vinkonu minni, Apríl!",
+    age: "0.5 ár (2025)",
+    funFact: "Zote the Mighty er uppáhalds persóna mín frá leiknum Hollow Knight",
+    about: "Mig hafði alltaf langað í þennan bangsa, svo þegar vinkona mín keypti hann fyrir mig á 18 ára afmæli mínu var ég himin lifandi. Ég hef mætt með hann í skólann áður, sérstaklega þegar ég er að mæta í próf því hann er ágætur andlegur stuðningur (og hvíslar mér svörin)."
+  },
+  {
+    id: "anthony",
+    name: "Anthony",
+    image: "/anthony.png",
+    obtained: "Keyptur í gjafabúð í dýragarði.",
+    age: "3 ára (2023)",
+    funFact: "Nafnið hans er borið fram ANT-hony (haha fattaru? hann er mauræta... ant..).",
+    about: "Mamma mín keypti hann fyrir mig eftir að við vorum að fara út úr dýragarði í Tenerife. Ég hef alltaf elskað muaraætur og fékk að koma rosa nálægt einni í garðinum, þannig að það er skemmtilegt að eiga litla minningu af því."
+  },
+  {
+    id: "jeffery",
+    name: "Jeffery",
+    image: "/jeffery.png",
+    obtained: "Keyptur í London í thrift búð",
+    age: "3 ára (2023)",
+    funFact: "Hann er afskaplega þægilegur að halda á þegar maður er liggjandi á hliðinni",
+    about: "Ég og mamma mín vorum í heimsókn hjá frænku minni um páskana árið 2023. Einn daginn fórum við ap thrifta og í einni búðinni kom ég auga á hann. Ég skildi hann næstum eftir en ég bara þurfti að hafa hann með haha. Gjaldkerinn lét meira að segja eins og ég sé að ættleiða hann þegar við fórum að borga."
+  },
+  {
+    id: "mallow",
+    name: "Mallow",
+    image: "/mallow.png",
+    obtained: "Óvænt gjöf frá pabba mínum.",
+    age: "1 árs (2025)",
+    funFact: "Mallow getur rúllað upp í kúlu!",
+    about: "Bangsi sem ég fékk óvænt frá pabba. Hann fór einn í IKEA og keypti hann handa mér, bara af engri ástæðu. Mér þykir mjög vænt um bangsanþ"
   }
 ];
 
 export default plushies;
+
